79db3a620914bd72ddae41577167d7bd706eda02
BRBX <egorka2005sautkin@gmail.com>
2024-06-05 15:04:47 +0300

    almost add tabs

--- app/src/main/java/com/example/caloriecounter/main_screens/screens/eating_screen/presentation/FeaturedDishes.kt
+++ app/src/main/java/com/example/caloriecounter/main_screens/screens/eating_screen/presentation/FeaturedDishes.kt
@@ -1,2 +1,11 @@
 package com.example.caloriecounter.main_screens.screens.eating_screen.presentation
 
+import androidx.compose.runtime.Composable
+import com.example.caloriecounter.main_screens.screens.eating_screen.data.meal_db.Meal
+
+@Composable
+fun FeaturedDishesContent(
+    dishes: List<Meal>
+) {
+
+}
