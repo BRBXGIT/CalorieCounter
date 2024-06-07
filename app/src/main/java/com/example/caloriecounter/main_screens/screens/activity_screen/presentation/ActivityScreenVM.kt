aa37a7e6df65cc8564182ffe642941096a880556
BRBX <egorka2005sautkin@gmail.com>
2024-06-07 23:53:17 +0300

    start creating add activity screen

---
+++ app/src/main/java/com/example/caloriecounter/main_screens/screens/activity_screen/presentation/ActivityScreenVM.kt
@@ -0,0 +1,4 @@
+package com.example.caloriecounter.main_screens.screens.activity_screen.presentation
+
+class ActivityScreenVM {
+}
---
+++ app/src/main/java/com/example/caloriecounter/main_screens/screens/activity_screen/presentation/AddActivityScreen.kt
@@ -0,0 +1,2 @@
+package com.example.caloriecounter.main_screens.screens.activity_screen.presentation
+
